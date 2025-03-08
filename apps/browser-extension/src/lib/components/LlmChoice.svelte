601b2bb89af6a704010647fa62c33f4ae10a04ce
Aleksander Grygier <aleksander.grygier@gmail.com>
2025-03-08 21:33:02 +0100

    feat: API key info

--- apps/browser-extension/src/lib/components/LlmChoice.svelte
+++ apps/browser-extension/src/lib/components/LlmChoice.svelte
@@ -58,6 +58,15 @@
 		<label>
 			<span>API Key</span>
 
+			<span>
+				Get your API key here:
+				{#if llmConfig.provider === 'google'}
+					<a href="https://aistudio.google.com/app/apikey" target="_blank">Google AI Studio</a>
+				{:else if llmConfig.provider === 'openai'}
+					<a href="https://platform.openai.com/api-keys" target="_blank">OpenAI Platform</a>
+				{/if}
+			</span>
+
 			<input
 				name="llm_api_key"
 				oninput={handleChangeValue}
