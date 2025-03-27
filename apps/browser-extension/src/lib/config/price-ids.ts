cc5b36bec41a8e96ec305c474b67ab8cd84860be
Aleksander Grygier <aleksander.grygier@gmail.com>
2025-03-27 01:42:46 +0100

    fix: Update price IDs

--- apps/browser-extension/src/lib/config/price-ids.ts
+++ apps/browser-extension/src/lib/config/price-ids.ts
@@ -1,17 +1,21 @@
 export const PRICE_IDS: {
-    name: string;
-    id: string;
-    paymentType: 'subscription' | 'one-time';
+	name: string;
+	id: string;
+	paymentType: 'subscription' | 'one-time';
 }[] = [
-        {
-            name: 'Monthly Subscription',
-            id: 'price_1R4gMiK3nWBBzoip2dxZIggk',
-            paymentType: 'subscription'
-        },
-        {
-            name: 'Annual Subscription',
-            id: 'price_1R6WPvK3nWBBzoipTAIDtDiQ',
-            paymentType: 'subscription'
-        },
-        { name: 'Lifetime License', id: import.meta.env.VITE_STRIPE_LIFETIME_PRICE_ID, paymentType: 'one-time' }
-    ];
+	{
+		name: 'Monthly Subscription',
+		id: 'price_1R74JFK3nWBBzoip3j2HqP0Z',
+		paymentType: 'subscription'
+	},
+	{
+		name: 'Yearly Subscription',
+		id: 'price_1R74JwK3nWBBzoipbL5SUeJH',
+		paymentType: 'subscription'
+	},
+	{
+		name: 'Lifetime License',
+		id: import.meta.env.VITE_STRIPE_LIFETIME_PRICE_ID,
+		paymentType: 'one-time'
+	}
+];
