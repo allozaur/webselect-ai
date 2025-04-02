add6fe81d57e39183be7b19968df01e718bf34b1
Aleksander Grygier <aleksander.grygier@gmail.com>
2025-04-02 19:07:28 +0200

    refactor: CustomerData type

--- apps/browser-extension/src/lib/stores/stripe-customer.ts
+++ apps/browser-extension/src/lib/stores/stripe-customer.ts
@@ -1,20 +1,5 @@
-import type Stripe from 'stripe';
 import { writable, type Writable } from 'svelte/store';
 
-const customer: Writable<{
-    customer: Stripe.Customer;
-    subscriptions: Stripe.Subscription[];
-    activeSubscription: {
-        subscriptionType: 'day' | 'week' | 'month' | 'year' | 'lifetime' | null;
-        isActive: boolean;
-        url: string | null;
-        period: {
-            start: number;
-            end: number;
-        } | null;
-        isTrial: boolean;
-        hadFinishedTrialsBefore: boolean;
-    } | null;
-} | null> = writable(null);
+const customer: Writable<CustomerData | null> = writable(null);
 
 export default customer;
